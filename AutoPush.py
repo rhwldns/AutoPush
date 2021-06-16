c98e2ba54789fc2da96328afa25a3c20df28a989
jjun4341 <42130391+jjun4341@users.noreply.github.com>
2021-06-17 08:58:39 +0900

    Update AutoPush.py

--- AutoPush.py
+++ AutoPush.py
@@ -1,5 +1,9 @@
 from os import popen
+import asyncio
 
-popen("git add .")
-popen('git commit -m "feat: some functions"')
-popen("git push")
+while True:
+  await asyncio.sleep(60 * 5)
+  popen("git add .")
+  popen('git commit -m "feat: some functions ㅣ AutoPush"')
+  popen("git push")
+  print("Finished pushing to Github. ㅣ Made By 땅콩")
